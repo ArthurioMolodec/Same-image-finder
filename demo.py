72f4ffb4cd4fddd4781bae96f5d1eb25e575bbbc
Артур <46633400+ArthurioMolodec@users.noreply.github.com>
2020-06-03 16:49:03 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,15 +6,16 @@ import SAME_IMAGE as si
 
 #imgs_search_in=ПАПКА С ИЗОБРАЖЕНИЯМИ ДЛЯ СРАВНЕНИЯ ИЛИ МАССИВ ПУТЕЙ К ИЗОБРАЖЕНИЯМИ ДЛЯ СРАВНЕНИЯ,
 
-#dist_multiplier=0.75,scaler=3,output=False)
+#dist_multiplier=0.75,scaler={МАКСИМАЛЬНАЯ ДЛИНА/ШИРИНА:[ДЛЯ ЧТО ИСКАТЬ, ДЛЯ ГДЕ ИСКАТЬ],-1 (ВСЁ ОСТАЛЬНОЕ):[ДЛЯ ЧТО ИСКАТЬ, ДЛЯ ГДЕ ИСКАТЬ]},output=False)
 
 
 
 
 # ДЛЯ ПОВЫШЕНИЯ ТОЧНОСТИ ТУТ ИСПОЛЬЗУЕТСЯ ФИЛЬТР ЗНАЧЕНИЙ, ПОЭТОМУ СКОРЕЕ ВСЕГО НОРМАЛЬНО РАБОТАТЬ С ОДНИМ ФАЙЛОМ, ЕСЛИ МНОГО ФАЙЛОВ, С КОТОРЫМ ЕГО НАДО СРАВНИТЬ, НЕ БУДЕТ
-#result = si.GetSame(imgs_orig=["what\\im1.png","what\\im2.png","what\\im3.png"],output=False)
-result = si.GetSame(output=False)
+# result = si.GetSame(imgs_orig=["what\\im1.png","what\\im2.png","what\\im3.png"],output=False)
 
+
+result = si.GetSame(scaler={50:[1.1, 1/1.1],-1:[2.46, 0.374]},dist_multiplier=0.75,output=False)
 print("")
 print("-"*55)
 print("dist_multiplier = ",result['dist_multiplier'])
@@ -23,4 +24,7 @@ for k in result['RESULT']:
 	print(k)
 print("-"*55)
 
+
+# result = si.GetSameImageAlgo(2)
+# print(result)
 input()
